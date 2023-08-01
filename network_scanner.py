ca982540a535061a87c695f816efba3dea3fbf04
utente <ghetto23@live.it>
2023-08-01 21:04:45 +0200

    print nmap results

--- network_scanner.py
+++ network_scanner.py
@@ -25,7 +25,7 @@ def get_local_ip():
 def scan_local_network(ip_address):
     ip_range = remove_last_octet(ip_address)
     # TODO: for debug original value 1, 254
-    start, end = 1, 2
+    start, end = 1, 254
     active_hosts = []
     for host in range(start, end):
         target_ip = ip_range + "." + str(host)
